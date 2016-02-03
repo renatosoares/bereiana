e092566e79443a2f0a62b75ce63cd367a74dbfee
Renato Soares <renatosoarespro@hotmail.com>
2016-02-03 17:29:57 -0300

    tudo foi alterado pelo chmod

--- header.php
+++ header.php
@@ -27,34 +27,43 @@
 	  <div id="load"></div>
 	</div>
 
-    <nav class="navbar navbar-custom navbar-fixed-top" role="navigation">
-        <div class="container">
-            <div class="navbar-header page-scroll">
-                <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-main-collapse">
-                    <i class="fa fa-bars"></i>
-                </button>
-                <a class="navbar-brand" href="index.php">
-                    <h1>BEREIANA</h1>
-                </a>
-            </div>
+  <nav class="navbar navbar-custom navbar-fixed-top" role="navigation">
+    <div class="container">
 
-            <!-- Collect the nav links, forms, and other content for toggling -->
-            <div class="collapse navbar-collapse navbar-right navbar-main-collapse">
-      <ul class="nav navbar-nav">
-        <li class="active"><a href="#intro">Home</a></li>
-        <li><a href="#posts">Notícias</a></li>
-		<li><a href="#contact">Contato</a></li>
-        <li class="dropdown">
-          <a href="#" class="dropdown-toggle" data-toggle="dropdown">Dropdown <b class="caret"></b></a>
-          <ul class="dropdown-menu">
-            <li><a href="#">Example menu</a></li>
-            <li><a href="#">Example menu</a></li>
-            <li><a href="#">Example menu</a></li>
-          </ul>
-        </li>
-      </ul>
-            </div>
-            <!-- /.navbar-collapse -->
-        </div>
-        <!-- /.container -->
-    </nav>
+
+
+      <div class="navbar-header page-scroll">
+        <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-main-collapse">
+          <i class="fa fa-bars"></i>
+        </button>
+        <a class="navbar-brand" href="index.php">
+          <h1>BEREIANA</h1>
+        </a>
+
+      </div>
+        <form class="navbar-form navbar-left" role="search">
+          <div class="input-group ">
+            <input type="text" class="form-control" placeholder="Search" style="width: 300px;">
+            <span class="input-group-btn">
+              <button class="btn btn-default" type="button">Go!</button>
+            </span>
+          </div>
+        </form>  <!-- Collect the nav links, forms, and other content for toggling -->
+      <div class="collapse navbar-collapse navbar-right navbar-main-collapse ">
+        <ul class="nav navbar-nav">
+          <li class="active"><a href="#intro">Home</a></li>
+          <li><a href="#posts">Notícias</a></li>
+          <li><a href="#contact">Contato</a></li>
+          <li class="dropdown">
+            <a href="#" class="dropdown-toggle" data-toggle="dropdown">Membros <b class="caret"></b></a>
+            <ul class="dropdown-menu">
+              <li><a href="member.php">Área do membro</a></li>
+              <li><a href="membership.php">Lista de membros</a></li>
+              <li><a href="#">Example menu</a></li>
+            </ul>
+          </li>
+        </ul>
+      </div> <!-- /.navbar-collapse -->
+
+    </div>  <!-- /.container -->
+  </nav>
---
+++ member.php
@@ -0,0 +1,22 @@
+<?php include 'header.php';?>
+<main class="container">
+  <section class="row">
+    <aside class="col-md-3">
+      <div class="thumbnail">
+        <img src="img/user.jpg" alt="usertest">
+        <div class="caption">
+          <h3>Thumbnail label</h3>
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis ac interdum leo. Aenean molestie metus at tellus malesuada, ut consequat est posuere.</p>
+          <p><a href="#" class="btn btn-primary" role="button">Button</a> <a href="#" class="btn btn-default" role="button">Button</a></p>
+        </div>
+      </div>
+    </aside>
+    <article class="col-md-9">
+      <p>
+        BBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBB
+      </p>
+    </article>
+  </section>
+
+</main>
+<?php include 'footer.php'; ?>
